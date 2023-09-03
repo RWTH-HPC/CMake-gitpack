2daf00261079f3d045770f3e353cd910c2ad7bb0
Sebastian Grabowski <grabowski@itc.rwth-aachen.de>
2023-09-03 20:18:13 +0200

    Require CMake 3.13.4 and above

--- cmake/GitPack.cmake
+++ cmake/GitPack.cmake
@@ -16,7 +16,7 @@
 #       no effect and it will behave just like the regular `CPack` file.
 
 # Set the minimum required CMake version.
-cmake_minimum_required(VERSION 2.8.12)
+cmake_minimum_required(VERSION 3.13.4...3.27.4)
 
 # Ignore some basic files, like the build and install directories and the git
 # directory (only neccessary, if packing from inside a git repositoy).
